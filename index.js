21fdc4898a091129f57739c9805548733e999e67
Aadil Sadik Mohmed Shaikh <ad979991@dal.ca>
2022-01-19 11:12:33 -0400

    Read Products Done

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const mongoClient = require("mongodb").MongoClient;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
 var connectionString =
   "mongodb+srv://aadil1:aadil123@cluster0.zehbr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -16,9 +18,23 @@ mongoClient
     console.log("Connected to database");
     const db = client.db("inventory-db");
     const collection = db.collection("inventory");
+    var items = [];
     app.get("/", (req, res) => {
-      res.sendFile(__dirname + "/main.html");
+      //   res.sendFile(__dirname + "/main.html");
+      const cursor = db
+        .collection("inventory")
+        .find()
+        .toArray()
+        .then((results) => {
+          console.log(results);
+          res.render("main.ejs", { items: results });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
     });
+
+    app.put("/updateproduct", (req, res) => {});
 
     app.get("/addproduct", (req, res) => {
       res.sendFile(__dirname + "/addproduct.html");

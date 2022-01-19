4f9108121cabd7b521d09100edc2e18c619a208f
Aadil Sadik Mohmed Shaikh <ad979991@dal.ca>
2022-01-19 02:08:26 -0400

    Add Product Done

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log("Hello world");
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const mongoClient = require("mongodb").MongoClient;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var connectionString =
+  "mongodb+srv://aadil1:aadil123@cluster0.zehbr.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+mongoClient
+  .connect(connectionString, {
+    useUnifiedTopology: true,
+  })
+  .then((client) => {
+    console.log("Connected to database");
+    const db = client.db("inventory-db");
+    const collection = db.collection("inventory");
+    app.get("/", (req, res) => {
+      res.sendFile(__dirname + "/main.html");
+    });
+
+    app.get("/addproduct", (req, res) => {
+      res.sendFile(__dirname + "/addproduct.html");
+    });
+
+    app.post("/addproduct", (req, res) => {
+      collection
+        .insertOne(req.body)
+        .then((result) => {
+          console.log(result);
+          res.redirect("/");
+        })
+        .catch((error) => console.error(error));
+    });
+    app.listen(5000, () => {
+      console.log("Server Started");
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
